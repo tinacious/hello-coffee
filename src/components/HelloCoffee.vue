8f4d932da4138ef283e947758e693bd91996ef31
Tina Holly <info@tinaciousdesign.com>
2019-02-24 16:29:06 -0500

    fetch content from contentful

---
+++ src/components/HelloCoffee.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+
+  </div>
+</template>
+
+<script>
+import { Coffee } from '../services/Coffee';
+
+Coffee.get().then((coffees) => {
+  console.table(coffees)
+})
+
+export default {
+  name: "HelloCoffee",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/services/Coffee.js
@@ -0,0 +1,20 @@
+import { createClient } from "contentful";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
+const client = createClient({
+  accessToken:
+    "4c33bdbdea25daf511701bae21468ed0a0c718974945ec383129fee266f9e1b4",
+  space: "3m3i9v8y9txq"
+});
+
+export const Coffee = {
+  get: () =>
+    client.getEntries().then(({ items }) =>
+      items
+        .map(({ fields }) => fields)
+        .map(({ description, ...rest }) => ({
+          ...rest,
+          description: documentToHtmlString(description)
+        }))
+    )
+};
